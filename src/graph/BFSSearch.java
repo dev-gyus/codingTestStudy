29896f498956c94852d5e57c2bc742318b17906e
GyuHyeong <cjsworbehd13@gmail.com>
2021-11-04 20:31:26 +0900

    1. 알고리즘 이론 학습

---
+++ src/graph/BFSSearch.java
@@ -0,0 +1,42 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 너비 우선 탐색을 수행하는 클래스(Breadth-First-Search)
+ */
+public class BFSSearch {
+    public ArrayList<String> bfsFunc(HashMap<String, ArrayList<String>> graph, String startNode){
+        ArrayList<String> visited = new ArrayList<>();
+        ArrayList<String> needVisit = new ArrayList<>();
+
+        needVisit.add(startNode);
+        while(needVisit.size() > 0){
+            String node = needVisit.remove(0);
+            if(!visited.contains(node)){
+                needVisit.addAll(graph.get(node));
+                visited.add(node);
+            }
+        }
+        return visited;
+    }
+    public static void main(String[] args){
+        HashMap<String, ArrayList<String>> graph = new HashMap<String, ArrayList<String>>();
+
+        graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<String>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<String>(Arrays.asList("I")));
+
+        BFSSearch bObject = new BFSSearch();
+        System.out.println(bObject.bfsFunc(graph, "A"));
+    }
+}
--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,25 +1,11 @@
 package sort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
- * 병합 정렬을 수행하기 전 주어진 Array를 2분할 하는 클래스 입니다.
+ * 병합 정렬을 수행하는 클래스
  */
 public class MergeSort {
-//    public void SplitFunc(List<Integer> tarList){
-//        // Size <= 1 일때는 별 다른 작업을 안해도 되므로 인자 바로 출력
-//        if(tarList.size() <= 1) System.out.println(tarList);
-//        // 주어진 인자 사이즈의 절반 값을 구함
-//        int medium = tarList.size()/2;
-//        // 위에서 구한 절반 값을 이용해 Array를 2개로 분할함
-//        List<Integer> leftArrays = tarList.subList(0, medium);
-//        List<Integer> rightArrays = tarList.subList(medium, tarList.size());
-//        // 분할한 리스트 toString 출력
-//        System.out.println(leftArrays + "\n" + rightArrays);
-//    }
-
     public ArrayList<Integer> mergeFunc(ArrayList<Integer> leftList, ArrayList<Integer> rightList){
         ArrayList<Integer> result = new ArrayList<>();
         int leftPoint = 0;
